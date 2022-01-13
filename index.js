e1845a32f562546cd5a70d15a4c4fb9d09b80aa7
Pandation <f.baumes@laposte.net>
2022-01-13 12:03:34 +0100

    deploy

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname,"./public/uploads/")));
 
 //REACT APP ADMIN OFFICE
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"/client/build/index.html"));
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
   // res.send({ response: "hello", url: __dirname + "/client/build/index.html" });
 });
 
