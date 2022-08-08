6b30cbab4c73de7b67c65aff6d1e934ce2c156e6
Florian Baumes <f.baumes@laposte.net>
2022-08-08 22:01:52 +0200

    new folder structure

--- backend/index.js
+++ backend/index.js
@@ -23,12 +23,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-console.log({
-  path: path.resolve(__dirname, './client/build/'),
-  join: path.join(__dirname, './client/build/'),
-  raw: './client/build'
-})
-app.use(express.static("public"));
+app.use(express.static("/public"));
 // app.use(express.static("/files/"));
 // app.use(express.static("/client/build/"));
 
@@ -56,46 +51,5 @@ app.listen(port, (err, doc) => {
   if (err) console.log(err);
   else {
     console.log("Server started on port " + port);
-    console.log(__dirname);
   }
 });
-
-//DEV
-// app.get("/test", (req, res) => {
-//   transporter.sendMail(
-//     {
-//       to: "mat.grimal@gmail.com",
-//       from: "florianbaumes@gmail.com",
-//       text: `
-//       Monsieur, Madame,
-
-//       Je vous écris cette lettre afin de postuler à l'annonce 20394 sur pôle emploi. En effet cela m'aiderait à nourrir les miens.
-//       En vous remerciant par avance, je vous prie d'agréer mes plus sincères salutations.
-
-//       Cordialement,
-//       Florian Baumes
-//       `,
-//       html: `
-//       <p>Monsieur, Madame,</p>
-//       <br/>
-//       <p>Je vous écris cette lettre afin de postuler à l'annonce 20394 sur pôle emploi. En effet cela m'aiderait à nourrir les miens.</p>
-//       <p>En vous remerciant par avance, je vous prie d'agréer mes plus sincères salutations.</p>
-//       <br />
-//       <p>Cordialement,</p>
-//       <p>Florian Baumes</p>
-//       `,
-//       subject: "Candidature spontanée",
-//       priority: "high",
-//       attachments: [
-//         {
-//           filename: "CV",
-//           path: path.join(__dirname, "./files", "CV.pdf"),
-//         },
-//       ],
-//     },
-//     (err, info) => {
-//       if (err) return console.log(err);
-//       console.log("mail envoyé si tout va bien :)");
-//     }
-//   );
-// });
