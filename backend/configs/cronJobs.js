58d196d3ee7f1afb6032920d4fba719543683f75
Florian Baumes <f.baumes@laposte.net>
2022-08-08 23:27:12 +0200

    new folder structure

--- backend/configs/cronJobs.js
+++ backend/configs/cronJobs.js
@@ -4,4 +4,3 @@ const nailedItCron = require("../services/nailedIt/cron/index");
 const testCron = require("../services/nailedIt/cron/test");
 
 let nailedIt = cron.job("0 */10 * * * *", testCron, null, true);
-
