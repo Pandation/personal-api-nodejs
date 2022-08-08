3c7f306b4b2d9b59c933be80a76cab32ff0eb71f
Florian Baumes <f.baumes@laposte.net>
2022-08-08 14:14:02 +0200

    test cron

--- backend/configs/cronJobs.js
+++ backend/configs/cronJobs.js
@@ -3,4 +3,4 @@ const cron = require("cron");
 const nailedItCron = require("../services/nailedIt/cron/index");
 const testCron = require("../services/nailedIt/cron/test");
 
-let nailedIt = cron.job("0 */10 * * * *", testCron, null, true);
+let nailedIt = cron.job("0 */10 * * * *", testCron, null, false);
