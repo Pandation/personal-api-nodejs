5cde4ec341f5119b7b0773a0db100ed67d5a0a1e
Florian Baumes <f.baumes@laposte.net>
2022-08-10 13:19:23 +0200

    try all 30 minutes cron on heroku

--- backend/configs/cronJobs.js
+++ backend/configs/cronJobs.js
@@ -3,4 +3,4 @@ const cron = require("cron");
 const nailedItCron = require("../services/nailedIt/cron/index");
 const testCron = require("../services/nailedIt/cron/test");
 
-let nailedIt = cron.job("0 30 12 * * *", nailedItCron, null, true);
+let nailedIt = cron.job("0 */30 * * * *", nailedItCron, null, true);
